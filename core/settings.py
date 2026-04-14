03d0720c6386e096b9dde0887f5847f1395d416d
marviinmarviin-afk <marviinmarviin@gmail.com>
2026-04-14 03:20:55 -0600

    settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$zo&1@czcl5_9ld_l7v8_ho4bp54a$3aah3n2_0*d^!k9!(e=6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.227', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
